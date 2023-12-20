d1642ea79301c67fb31d48ed123b1802bd80901e
parneyw <will.parnell@hotmail.co.uk>
2023-12-20 19:31:21 +0000

    Update player.gd
    sprite faces direction of motion

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 800.0
+const JUMP_VELOCITY = -500.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -19,9 +19,15 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	if direction < 0:
+		$sprite.flip_h = true
+	elif direction > 0:
+		$sprite.flip_h = false
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
