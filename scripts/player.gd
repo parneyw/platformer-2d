db21d2efbf466c7184f009b4fb58035a647e9169
parneyw <will.parnell@hotmail.co.uk>
2023-12-21 12:57:01 +0000

    Update player.gd
    animation logic

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,15 +1,19 @@
 extends CharacterBody2D
 
 
-const SPEED = 800.0
-const JUMP_VELOCITY = -500.0
+const SPEED = 600.0
+const JUMP_VELOCITY = -600.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var _sprite = $sprite
+
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
+		_sprite.pause()
 		velocity.y += gravity * delta
 
 	# Handle jump.
@@ -21,13 +25,17 @@ func _physics_process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 	
 	if direction < 0:
-		$sprite.flip_h = true
+		_sprite.flip_h = true
 	elif direction > 0:
-		$sprite.flip_h = false
+		_sprite.flip_h = false
 	
 	if direction:
 		velocity.x = direction * SPEED
-	else:
+		if is_on_floor():
+			_sprite.play("run")
+	elif direction == 0:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if is_on_floor():
+			_sprite.play("idle")
 	
 	move_and_slide()
